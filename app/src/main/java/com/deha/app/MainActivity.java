c8d34611f6cd8e3f6de207536d457a164d850eda
halloga <halloga00@gmail.com>
2020-05-29 21:36:47 +0300

    add new permissions

--- app/src/main/java/com/deha/app/MainActivity.java
+++ app/src/main/java/com/deha/app/MainActivity.java
@@ -30,148 +30,144 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
 
-    public static final int LOCATION_PERMISSION_REQUEST_CODE = 11;
-    public static final String DEHA_NEEDS_LOC_PERM = "DeHA'nın çalışabilmesi için telefonunuzun lokasyonuna ihtiyacı var. DeHA'yı kullanmak için bu izini vermeniz gerekiyor.";
-    public static String userId;
+  public static final int PERMISSION_REQUEST_CODE = 11;
+  public static final String DEHA_NEEDS_LOC_PERM = "DeHA'nın çalışabilmek için konum, medya ve rehber erişimine ihtiyacı var.";
+  public static String userId;
 
-    private boolean isSettingsTapped = false;
-    private ActivityMainBinding binding;
-    String[] fineLocationPerm = { Manifest.permission.ACCESS_FINE_LOCATION};
+  private boolean isSettingsTapped = false;
+  private ActivityMainBinding binding;
+  String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setLifecycleOwner(this);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    binding.setLifecycleOwner(this);
 
-        LocalStorageService localStorageService = new LocalStorageService(getSharedPreferences(getPackageName(), MODE_PRIVATE));
-        if (localStorageService.getUserId() == null) {
-            localStorageService.setUserId(Utils.getUuid());
+    LocalStorageService localStorageService = new LocalStorageService(getSharedPreferences(getPackageName(), MODE_PRIVATE));
+    if (localStorageService.getUserId() == null) {
+      localStorageService.setUserId(Utils.getUuid());
+    }
+
+    userId = localStorageService.getUserId();
+
+    checkPermissions();
+  }
+
+  private void checkPermissions() {
+    if (EasyPermissions.hasPermissions(this, permissions)) {
+      navigateToDiscoverMapFragment();
+    } else {
+        askForPermission();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (isSettingsTapped) {
+      isSettingsTapped = false;
+      checkPermissions();
+    }
+  }
+
+  public void navigateBroadcastMessageFragment() {
+    binding.progress.setVisibility(View.GONE);
+    FragmentUtils.replaceFragment(getSupportFragmentManager(),
+        BroadcastMessageFragment.newInstance(), R.id.container, "broadcastmessage");
+  }
+
+  public void navigateToHouseLocationFragment() {
+    binding.progress.setVisibility(View.GONE);
+    FragmentUtils.replaceFragment(getSupportFragmentManager(),
+        HouseLocationFragment.newInstance(false, null), R.id.container, "householdinfo");
+  }
+
+  public void navigateToDiscoverMapFragment() {
+    binding.progress.setVisibility(View.GONE);
+    List<UserModel> users = new ArrayList<>();
+    users.add(new UserModel("1", "akbas", 39.9209483, 32.8277882, 1));
+    users.add(new UserModel("2", "akbas2", 39.9219483, 32.8277882, 1));
+    users.add(new UserModel("3", "akbas3", 39.9229483, 32.8277882, 1));
+    users.add(new UserModel("4", "akbas4", 39.9239483, 32.8277882, 1));
+    RequestModel requestModel = new RequestModel("0", 39.9234809, 32.8197219, users);
+    FragmentUtils.replaceFragment(getSupportFragmentManager(),
+        DiscoverMapFragment.newInstance(requestModel.toJson()), R.id.container, "discovermap");
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (getSupportFragmentManager().getBackStackEntryCount() == 1) {
+      finish();
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    // Forward results to EasyPermissions
+    EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+  }
+
+  private Fragment getCurrentFragment() {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    int index = fragmentManager.getBackStackEntryCount() - 1;
+    FragmentManager.BackStackEntry backEntry = fragmentManager.getBackStackEntryAt(index);
+    String tag = backEntry.getName();
+    return fragmentManager.findFragmentByTag(tag);
+  }
+
+  @Override
+  public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+    checkPermissions();
+  }
+
+  @Override
+  public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+    if (requestCode == PERMISSION_REQUEST_CODE) {
+      if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+        if (!EasyPermissions.hasPermissions(this, permissions)) {
+          isSettingsTapped = true;
+          new AppSettingsDialog.Builder(this)
+              .setRationale("DeHA'nın çalışabilmek için konum, medya ve rehber erişimine ihtiyacı var.")
+              .setPositiveButton("Ayarlara git")
+              .setNegativeButton("İptal")
+              .setTitle("Uyarı").build().show();
         }
-
-        userId = localStorageService.getUserId();
-
-        if (EasyPermissions.hasPermissions(this, fineLocationPerm)) {
-            navigateBroadcastMessageFragment();
-        } else {
-            askForLocationPermission();
-        }
+      } else {
+          askForPermission();
+      }
     }
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(isSettingsTapped){
-            if (EasyPermissions.hasPermissions(this, fineLocationPerm)) {
-                navigateBroadcastMessageFragment();
-            } else {
-                askForLocationPermission();
-            }
+  @Override
+  public void onRationaleAccepted(int requestCode) {
+      askForPermission();
+  }
 
-        }
-    }
+  @Override
+  public void onRationaleDenied(int requestCode) {
+    Handler handler = new Handler(getMainLooper());
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        handler.postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              askForPermission();
+          }
+        }, 1000);
+      }
+    }).start();
+  }
 
-    public void navigateBroadcastMessageFragment() {
-        binding.progress.setVisibility(View.GONE);
-        FragmentUtils.replaceFragment(getSupportFragmentManager(),
-                BroadcastMessageFragment.newInstance(), R.id.container, "broadcastmessage");
-    }
+  private void askForPermission() {
+    EasyPermissions.requestPermissions(this, DEHA_NEEDS_LOC_PERM,
+        PERMISSION_REQUEST_CODE, permissions);
 
-    public void navigateToHouseLocationFragment() {
-        binding.progress.setVisibility(View.GONE);
-        FragmentUtils.replaceFragment(getSupportFragmentManager(),
-                HouseLocationFragment.newInstance(false, null), R.id.container, "householdinfo");
-    }
-
-    public void navigateToDiscoverMapFragment() {
-        binding.progress.setVisibility(View.GONE);
-        List<UserModel> users = new ArrayList<>();
-        users.add(new UserModel("1", "akbas", 39.9209483, 32.8277882, 1));
-        users.add(new UserModel("2", "akbas2", 39.9219483, 32.8277882, 1));
-        users.add(new UserModel("3", "akbas3", 39.9229483, 32.8277882, 1));
-        users.add(new UserModel("4", "akbas4", 39.9239483, 32.8277882, 1));
-        RequestModel requestModel = new RequestModel("0", 39.9234809, 32.8197219, users);
-        FragmentUtils.replaceFragment(getSupportFragmentManager(),
-                DiscoverMapFragment.newInstance(requestModel.toJson()), R.id.container, "discovermap");
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(getSupportFragmentManager().getBackStackEntryCount() == 1){
-            finish();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // Forward results to EasyPermissions
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    private Fragment getCurrentFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        int index = fragmentManager.getBackStackEntryCount() - 1;
-        FragmentManager.BackStackEntry backEntry = fragmentManager.getBackStackEntryAt(index);
-        String tag = backEntry.getName();
-        return fragmentManager.findFragmentByTag(tag);
-    }
-
-    @Override
-    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
-        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE){
-            navigateBroadcastMessageFragment();
-        }
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
-
-        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE){
-            if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-                if (!EasyPermissions.hasPermissions(this, fineLocationPerm)) {
-                    isSettingsTapped = true;
-                    new AppSettingsDialog.Builder(this)
-                            .setRationale("DeHA'nın çalışabilmesi için telefonunuzun lokasyonuna ihtiyacı var.")
-                            .setPositiveButton("Ayarlara git")
-                            .setNegativeButton("İptal")
-                            .setTitle("Uyarı").build().show();
-                }
-            }
-            else{
-                askForLocationPermission();
-            }
-        }
-    }
-
-    @Override
-    public void onRationaleAccepted(int requestCode) {
-        askForLocationPermission();
-    }
-
-    @Override
-    public void onRationaleDenied(int requestCode) {
-        Handler handler = new Handler(getMainLooper());
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        askForLocationPermission();
-                    }
-                }, 1000);
-            }
-        }).start();
-    }
-
-    private void askForLocationPermission(){
-        EasyPermissions.requestPermissions(this, DEHA_NEEDS_LOC_PERM,
-                LOCATION_PERMISSION_REQUEST_CODE, fineLocationPerm);
-
-    }
+  }
 }
 
